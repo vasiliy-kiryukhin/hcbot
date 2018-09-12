e37ba4d7eff5a140e512dc6f1d53766d7f3d2f9a
polosaty <vasek_k2000@mail.ru>
2018-09-12 12:44:30 +0300

    stable 0.0.1 for work with Heroku

--- src/HCBot.Runner/Program.cs
+++ src/HCBot.Runner/Program.cs
@@ -1,8 +1,12 @@
 ﻿using HCBot.Runner.Schedule;
 using HCBot.Runner.States;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -23,48 +27,68 @@ namespace HCBot.Runner
         public const string botKey = "665509241:AAFxRlJKv4E-NoYMZvGWuu7o7OleN9a-4hI";
         public const string apiBotKey = "bot" + botKey;
         private Dictionary<long, UserStateBag> user = new Dictionary<long, UserStateBag>();
+        public ManualResetEvent botHaltEvent = new ManualResetEvent(false);
 
+        ILogger logger;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
             Task.Run(() =>
             {
                 var p = new Program();
                 p.Run();
-            }).Wait();
 
+            }).Wait();
+            
 
         }
 
         void Run()
         {
+            ILoggerFactory loggerFactory =
+                new LoggerFactory()
+                    .AddConsole()
+                    .AddDebug();
+            logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogInformation("HCbot started");
+
             var bot = new TelegramBotClient(botKey);
-          
-
-            bot.StartReceiving();
-            bot.OnMessage += Bot_OnMessage;
-
-            Console.ReadKey();
-
-            bot.StopReceiving();
+                               
+            logger.LogInformation("HCbot awaiting halt event");
+            try
+            {
+                bot.StartReceiving();
+                bot.OnMessage += Bot_OnMessage;
+                botHaltEvent.WaitOne();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error while WaitOne");
+            }
+            finally
+            {
+                bot.StopReceiving();
+                logger.LogInformation("HCbot halt");
+            }
         }
 
 
         private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-
-            
-
-            ExecuteCommand(sender as TelegramBotClient, e.Message.Chat, e.Message.Text);
-           
-
-            Console.WriteLine(e.Message);
+            logger.LogInformation($"Message received from {e.Message.Chat.Id} : {e.Message.Text}");
+            ExecuteCommand(sender as TelegramBotClient, e.Message.Chat, e.Message.Text);            
         }
 
         private void ExecuteCommand(ITelegramBotClient bot, Chat chat, string commandName)
         {
+            if (commandName=="halt")
+            {
+                logger.LogInformation($"Setting halt event");
+                botHaltEvent.Set();
+                return;
+            }
             if (!user.ContainsKey(chat.Id))
             {
                 user.Add(chat.Id, new UserStateBag { UserState = UserBotState.SerfMenu, BotMenu = BotMenu.LoadFromFile("Structure.json") });
